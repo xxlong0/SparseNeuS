7c5e1d47283c07583e665e35fe8c4ce699facb8a
xiaoxiao <longxiao1115@outlook.com>
2022-09-22 23:07:34 +0800

    fix wrong ft parameters

--- sample_bashs/dtu_scan118.sh
+++ sample_bashs/dtu_scan118.sh
@@ -3,4 +3,4 @@ python exp_runner_finetune.py \
 --mode train --conf ./confs/finetune.conf --is_finetune \
 --checkpoint_path ./weights/ckpt.pth \
 --case_name scan118  --train_imgs_idx 0 1 2 --test_imgs_idx 0 1 2 --near 700 --far 1100 \
---visibility_beta 0.015 --visibility_gama 0.010 --visibility_weight_thred 0.7
+--visibility_beta 0.025 --visibility_gama 0.010 --visibility_weight_thred 0.7 0.6 0.5
